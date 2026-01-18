a4c0a907629bf2b46c87a31a31be2eab57af94be
arohao <119353048+arohao@users.noreply.github.com>
2026-01-18 06:38:48 -0500

    deployaway

--- api/index.js
+++ api/index.js
@@ -178,9 +178,16 @@ app.options('*', cors(corsOptions));
 // Add CORS headers to all responses as fallback (for Vercel serverless functions)
 app.use((req, res, next) => {
   const origin = req.headers.origin;
-  // Always set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', origin || '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  
+  // If there's an origin header, use it (required when credentials: true)
+  // Otherwise allow all origins
+  if (origin) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+  } else {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+  }
+  
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-API-Key, Accept');
   
